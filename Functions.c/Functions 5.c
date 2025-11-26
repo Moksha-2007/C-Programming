31fa3ef3caf089fb061294dda8d8b028e4405b50
Moksha-2007 <25kq1a5475@pace.ac.in>
2025-11-26 23:57:33 +0530

    Create Functions 5.c

---
+++ Functions.c/Functions 5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int reverse_number(int n);
+int main() 
+{
+    int original_num, reversed_num;
+    printf("Enter an integer to reverse: ");
+    scanf("%d", &original_num);
+    reversed_num = reverse_number(original_num);
+    printf("The original number is: %d\n", original_num);
+    printf("The reversed number is: %d\n", reversed_num);
+    return 0;
+}
+int reverse_number(int n) 
+{
+    int reversed = 0;
+    int remainder;
+    while (n != 0) 
+    {
+        remainder = n % 10;      
+        reversed = reversed * 10 + remainder; 
+        n /= 10;                 
+    }
+    return reversed; 
+}
+
+
+
+
