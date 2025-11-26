71daebf4ffef5809c5c15693488dfbf5210f504a
Moksha-2007 <25kq1a5475@pace.ac.in>
2025-11-26 23:34:07 +0530

    Create PrimeOrNotPrime.c

---
+++ Functions.c/PrimeOrNotPrime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h> 
+int Prime(int number)
+{
+    if (number <= 1) 
+    {
+        return 0;
+}
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return 0; 
+        }
+    }
+    return 1; 
+}
+int main() 
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (Prime(num)) 
+    {
+        printf("%d is a prime number.\n", num);
+    }
+        else 
+    {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
