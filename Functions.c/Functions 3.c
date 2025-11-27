fa30ad81f1c87157dda5d9c23f67acc5a8a40cbc
Moksha-2007 <25kq1a5475@pace.ac.in>
2025-11-27 19:49:07 +0530

    Update Functions 3.c

--- Functions.c/Functions 3.c
+++ Functions.c/Functions 3.c
@@ -7,8 +7,10 @@ int Prime(int number)
     {
         return 0;
 }
-    for (int i = 2; i * i <= number; i++) {
-        if (number % i == 0) {
+    for (int i = 2; i * i <= number; i++)
+        {
+        if (number % i == 0)
+        {
             return 0; 
         }
     }
